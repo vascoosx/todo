be49165fa5b35b9f4a4f46594ccb86802be46580
Shunichi Hato <shunichi@meuron.jp>
2020-05-10 19:19:32 +0900

    enable checking

--- todo.js
+++ todo.js
@@ -7,7 +7,11 @@ class App {
     this.target = options.target;
   }
   addItem(item) {
-    this.items = [...this.items, item]
+    this.items = [...this.items, {value: item, checked: false}]
+  }
+  checkItem(i, status) {
+    this.items[i].checked = status;
+    this.show();
   }
   deleteItem(i) {
     this.items.splice(i, 1)
@@ -22,11 +26,13 @@ class App {
   }
   mainTemplate() {
     return html`
-    <div class="flex justify-center bg-gray-200 h-screen">
-      <div class="flex flex-col p-2 border-solid w-3/12 rounded bg-white h-screen mt-4">
-        <h1 class="font-bold p-2">What needs to be done?</h1>
-        <input class="border-2 m-2 p-1" type="text" @keypress=${this.handleNewItem}>
-        ${this.items.length > 0 ? this.listTemplate() : ''}
+    <div class="h-screen bg-gray-200">
+      <div class="flex justify-center p-4">
+        <div class="flex flex-col p-2 border-solid w-3/12 rounded bg-white mt-4">
+          <h1 class="font-bold p-2">What needs to be done?</h1>
+          <input class="border-2 m-2 p-1" type="text" @keypress=${this.handleNewItem}>
+          ${this.items.length > 0 ? this.listTemplate() : ''}
+        </div>
       </div>
     </div>
     `
@@ -41,8 +47,10 @@ class App {
   singleItem(x, i) {
     return html`
       <li class="px-2 flex justify-between border-b p-2">
-        <input type="checkbox" class="mx-1" />
-        <label class="w-full mx-2 break-all">${x}</label>
+        <input type="checkbox" class="mx-1" .checked=${x.checked} @change=${e => this.checkItem(i, e.currentTarget.checked)} />
+        ${x.checked ? 
+          html`<label class="w-full mx-2 break-all line-through">${x.value}</label>` : 
+          html`<label class="w-full mx-2 break-all">${x.value}</label>`}
         <button class="flex-none block w-4 mx-2 font-sans" @click=${_e => this.deleteItem(i)}>x</button>
       </li>
     `
@@ -52,5 +60,5 @@ class App {
   }
 }
 
-const app = new App({target: document.body, items: ['sample']});
+const app = new App({target: document.body, items: [{value: 'sample', checked: false}]});
 app.show();
